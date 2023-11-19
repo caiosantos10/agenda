faba819a99aa237d31ce939c2881ed0e84c85262
Caio Santos <caiosant10@gmail.com>
2023-11-19 17:56:30 -0300

    Listagem simples com tableView

--- agenda/ViewController.swift
+++ agenda/ViewController.swift
@@ -9,28 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var myView: View = {
-        let view = View()
-        view.delegate = self
-        return view
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .systemBackground
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableView
     }()
     
     override func loadView() {
         super.loadView()
+        setupUI()
         
-        self.view = myView
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad();
+    func setupUI() {
+        self.view.backgroundColor = .systemBlue
         
-        myView.setup(labelText: "Olá mundo", buttonTitle: "Testar")
+        self.view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        
     }
 }
 
-extension ViewController: ViewDelegate {
-    func didTapButton() {
-        myView.setup(labelText: "Sucesso", buttonTitle: "Testar novamente")
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Lara Antonio"
+        return cell;
+    }
+    
+    
 }
 
